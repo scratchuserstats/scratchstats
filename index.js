34fdb66abd75ce778023b42fb105a84ffca7c728
Sheshank Shankar <sheshank@outlook.com>
2017-08-23 16:58:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ swal({
   confirmButtonText: 'Cool'
 })
 */
+
+var followers;
+
+function userStatsUpdate (username) {
+  followers(username);
+}
+
+function followers(username) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'https://scratch.mit.edu/users/' + username + '/followers/', true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var response  = xmlhttp.responseText;
+            var find = response.search("<h2>");
+            followers = response.substring(find,find+200).match(/\(([^)]+)\)/)[1];
+
+            document.getElementById("followers").innerHTML = followers;
+          }
+    }
+
+}
