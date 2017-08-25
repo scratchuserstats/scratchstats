3be2c4d3b38bc4c9a2a44f6e6ea69b0412477d8c
World_Languages <mundofinky@gmail.com>
2017-08-25 16:45:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var followers;
 var following;
-var exampleLocation = [
-  'United States',	'Afghanistan',	'Aland Islands',	'Albania',	'Algeria',	'American Samoa',	'Andorra',	'Angola',	'Anguilla',	'Antarctica',	'Antigua and Barbuda',	'Argentina',	'Armenia',	'Aruba',	'Australia',	'Austria',	'Azerbaijan',	'Bahamas',	'Bonaire, Sint Eustatius and Saba',	'Bahrain',	'Bangladesh',	'Barbados',	'Belarus',	'Belgium',	'Belize',	'Benin',	'Bermuda',	'Bhutan',	'Bolivia',	'Bosnia and Herzegovina',	'Botswana',	'Bouvet Island',	'Brazil',	'British Indian Ocean Territory',	'Brunei',	'Bulgaria',	'Burkina Faso',	'Burundi',	'Cambodia',	'Cameroon',	'Canada',	'Cape Verde',	'Cayman Islands',	'Central African Republic',	'Chad',	'Chile',	'China',	'Christmas Island',	'Cocos (Keeling) Islands',	'Colombia',	'Comoros',	'Congo',	'Congo, The Democratic Republic of the',	'Cook Islands',	'Costa Rica',	"Cote d'Ivoire",	'Croatia',	'Cuba',	'Cyprus',	'Czech Republic',	'Denmark',	'Djibouti',	'Dominica',	'Dominican Republic',	'Ecuador',	'Egypt',	'El Salvador',	'Equatorial Guinea',	'Eritrea',	'Estonia',	'Ethiopia',	'Falkland Islands (Malvinas)',	'Faroe Islands',	'Fiji',	'Finland',	'France',	'French Guiana',	'French Polynesia',	'French Southern Territories',	'Gabon',	'Gambia',	'Georgia',	'Germany',	'Ghana',	'Gibraltar',	'Greece',	'Greenland',	'Grenada',	'Guadeloupe',	'Guam',	'Guatemala',	'Guernsey',	'Guinea',	'Guinea-Bissau',	'Guyana',	'Haiti',	'Heard Island and McDonald Islands',	'Vatican City',	'Honduras',	'Hong Kong',	'Hungary',	'Iceland',	'India',	'Indonesia',	'Iran',	'Iraq',	'Ireland',	'Isle of Man',	'Israel',	'Italy',	'Jamaica',	'Japan',	'Jersey',	'Jordan',	'Kazakhstan',	'Kenya',	'Kiribati',	'North Korea',	'South Korea',	'Kuwait',	'Kyrgyzstan',	'Laos',	'Latvia',	'Lebanon',	'Lesotho',	'Liberia',	'Libya',	'Liechtenstein',	'Lithuania',	'Luxembourg',	'Macao',	'Macedonia, The Former Yugoslav Republic of',	'Madagascar',	'Malawi',	'Malaysia',	'Maldives',	'Mali',	'Malta',	'Marshall Islands',	'Martinique',	'Mauritania',	'Mauritius',	'Mayotte',	'Mexico',	'Micronesia, Federated States of',	'Moldova',	'Monaco',	'Mongolia',	'Montenegro',	'Montserrat',	'Morocco',	'Mozambique',	'Myanmar',	'Namibia',	'Nauru',	'Nepal',	'Netherlands',	'Netherlands Antilles',	'New Caledonia',	'New Zealand',	'Nicaragua',	'Niger',	'Nigeria',	'Niue',	'Norfolk Island',	'Northern Mariana Islands',	'Norway',	'Oman',	'Pakistan',	'Palau',	'Palestine, State of',	'Panama',	'Papua New Guinea',	'Paraguay',	'Peru',	'Philippines',	'Pitcairn',	'Poland',	'Portugal',	'Puerto Rico',	'Qatar',	'Reunion',	'Romania',	'Russia',	'Rwanda',	'Saint Barthelemy',	'Saint Helena',	'Saint Kitts and Nevis',	'Saint Lucia',	'Saint Martin',	'Saint Pierre and Miquelon',	'Saint Vincent and the Grenadines',	'Samoa',	'San Marino',	'Sao Tome and Principe',	'Saudi Arabia',	'Senegal',	'Serbia',	'Seychelles',	'Sierra Leone',	'Singapore',	'Slovakia',	'Slovenia',	'Solomon Islands',	'Somalia',	'South Africa',	'South Georgia and the South Sandwich Islands',	'South Sudan',	'Spain',	'Sri Lanka',	'Sudan',	'Suriname',	'Svalbard and Jan Mayen',	'Swaziland',	'Sweden',	'Switzerland',	'Syria',	'Taiwan',	'Tajikistan',	'Tanzania',	'Thailand',	'Timor-Leste',	'Togo',	'Tokelau',	'Tonga',	'Trinidad and Tobago',	'Tunisia',	'Turkey',	'Turkmenistan',	'Turks and Caicos Islands',	'Tuvalu',	'Uganda',	'Ukraine',	'United Arab Emirates',	'United Kingdom',	'United States Minor Outlying Islands',	'Uruguay',	'Uzbekistan',	'Vanuatu',	'Venezuela',	'Vietnam',	'Virgin Islands, British',	'Virgin Islands, U.S.',	'Wallis and Futuna',	'Kosovo',	'Western Sahara',	'Yemen',	'Zambia',	'Zimbabwe'];
 function userStatsUpdate(username) {
     console.log(username);
     var xmlhttp = new XMLHttpRequest();
@@ -16,7 +14,7 @@ function userStatsUpdate(username) {
             document.getElementById('icon').src = src;
             document.getElementById('user').innerHTML =  "<b>@" + obj.username + "</b>" + "</a>";
         }
-    }
+    };
     followers(username);
 }
 
@@ -28,12 +26,12 @@ function followers(username) {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             var response = xmlhttp.responseText;
             var find = response.search("<h2>");
-            followers = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
+            var followers = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
 
             document.getElementById("followers").innerHTML = followers;
             following(username);
         }
-    }
+    };
 
 }
 
@@ -45,12 +43,12 @@ function following(username) {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             var response = xmlhttp.responseText;
             var find = response.search("<h2>");
-            following = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
+            var following = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
 
             document.getElementById("following").innerHTML = following;
             avgFollows(username);
         }
-    }
+    };
 
 }
 
@@ -81,16 +79,16 @@ function avgFollows(username) {
             avgFollowsPerHour = avgFollowsPerHour.toFixed(2);
 
             if (year < 1) {
-              document.getElementById('avgFollowersYear').style.fontSize = "x-large";
+                document.getElementById('avgFollowersYear').style.fontSize = "x-large";
                 document.getElementById('avgFollowersYear').innerHTML = 'Error: User under one year';
                 if ((curDate.getUTCMonth() + 1) - month == 0) {
-                  document.getElementById('avgFollowersMonth').style.fontSize = "x-large";
+                    document.getElementById('avgFollowersMonth').style.fontSize = "x-large";
                     document.getElementById('avgFollowersMonth').innerHTML = 'Error: User is newer than a month';
                     if (curDate.getUTCDate() == day) {
-                      document.getElementById('avgFollowersDay').style.fontSize = "x-large";
+                        document.getElementById('avgFollowersDay').style.fontSize = "x-large";
                         document.getElementById('avgFollowersDay').innerHTML = 'Error: User is newer than a day';
                         if (curDate.getUTCHours() == hour) {
-                          document.getElementById('avgFollowersHour').style.fontSize = "x-large";
+                            document.getElementById('avgFollowersHour').style.fontSize = "x-large";
                             document.getElementById('avgFollowersDay').innerHTML = 'Error: User is newer than a hour';
                         } else {
                             document.getElementById('avgFollowersHour').innerHTML = avgFollowsPerHour;
@@ -101,7 +99,7 @@ function avgFollows(username) {
                     }
                 } else if ((curDate.getUTCMonth() + 1) - month == 1) {
                     if (curDate.getUTCDate() < day) {
-                      document.getElementById('avgFollowersMonth').style.fontSize = "x-large";
+                        document.getElementById('avgFollowersMonth').style.fontSize = "x-large";
                         document.getElementById('avgFollowersMonth').innerHTML = 'Error: User is newer than a month';
                         document.getElementById('avgFollowersHour').innerHTML = avgFollowsPerHour;
                         document.getElementById('avgFollowersDay').innerHTML = avgFollowsPerDay;
@@ -123,46 +121,61 @@ function avgFollows(username) {
             }
             idNumber(username);
         }
-    }
+    };
 }
 
 function idNumber(username) {
     var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', 'https://api.scratch.mit.edu/users/' + username, true);
-  xmlhttp.send();
-  xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        var response = xmlhttp.responseText;
-        var obj = JSON.parse(response);
-        document.getElementById('id').innerHTML = obj.id;
-        messageCount(username);
-      }
-    }
+    xmlhttp.open('GET', 'https://api.scratch.mit.edu/users/' + username, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var response = xmlhttp.responseText;
+            var obj = JSON.parse(response);
+            document.getElementById('id').innerHTML = obj.id;
+            messageCount(username);
+        }
+    };
 }
 
 function messageCount(username) {
-  if (username.toLowerCase() != "griffpatch"){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET','https://api.scratch.mit.edu/users/'+username+'/messages/count',true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        var response = xmlhttp.responseText;
-        var obj = JSON.parse(response);
-        document.getElementById('messageCount').innerHTML = obj.count;
-      }
-  }
-}
-else {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET','https://api.scratch.mit.edu/proxy/users/griffpatch/activity/count?'+Math.floor(Date.now() / 1000),true);
-  xmlhttp.send();
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      var response = xmlhttp.responseText;
-      var obj = JSON.parse(response);
-      document.getElementById('messageCount').innerHTML = obj.msg_count;
+    activity(username);
+    if (username.toLowerCase() !== "griffpatch"){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET','https://api.scratch.mit.edu/users/'+username+'/messages/count',true);
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                var response = xmlhttp.responseText;
+                var obj = JSON.parse(response);
+                document.getElementById('messageCount').innerHTML = obj.count;
+            }
+        };
+    }
+    else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET','https://api.scratch.mit.edu/proxy/users/griffpatch/activity/count?'+Math.floor(Date.now() / 1000),true);
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                var response = xmlhttp.responseText;
+                var obj = JSON.parse(response);
+                document.getElementById('messageCount').innerHTML = obj.msg_count;
+            }
+        };
     }
 }
-}
+
+function activity() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'https://scratch.mit.edu/messages/ajax/user-activity/?user=' + username + '&max=1000000', true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var responseactivity  = xmlhttp.responseText;
+            var countloves = (responseactivity.match(/icon-xs black love/g) || []).length;
+            document.getElementById("amtOfLovedProjects").innerHTML = countloves;
+            /*var countshares = (responseactivity.match(/icon-xs black project/g) || []).length;
+            document.getElementById("unsharedprojs").innerHTML = "Projects that were unshared: <b>" + String(countshares-totalProjects) + "</b> (BETA)";*/
+        }};
 }
